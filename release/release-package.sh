877fd764dd535871c88cd2faba977fd5e3fcc963
Colleen McGuckin <colleenmcguckin@gmail.com>
2019-05-02 13:36:45 -0700

    exit instead of echo

--- release/release-package.sh
+++ release/release-package.sh
@@ -5,9 +5,9 @@ ref_type=$(jq '.ref_type' ${GITHUB_EVENT_PATH})
 if [[ $ref_type == "\"tag"\" ]]
 then
   echo "its a tag"
-  echo 0
+  exit 0
 else
   echo "it is not a tag"
   # do not continue the workflow
-  echo 42
+  exit 42
 fi
