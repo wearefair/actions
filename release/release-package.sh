1a3b4c88f4e3c90248e178234b7d009c814bfc34
Colleen McGuckin <colleenmcguckin@gmail.com>
2019-05-09 14:45:25 -0700

    see artifacts

--- release/release-package.sh
+++ release/release-package.sh
@@ -6,7 +6,10 @@ if [[ $ref_type == "\"tag"\" ]]
 then
   echo "its a tag"
   python setup.py bdist_wheel
+  echo "**************"
   ls
+  echo "**************"
+  ls dist
   exit 0
 else
   echo "it is not a tag"
