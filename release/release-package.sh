b94ea32cadaea22e43bf52ad7abff2e627ad8683
Colleen McGuckin <colleenmcguckin@gmail.com>
2019-05-09 17:47:32 -0700

    try to create a release

--- release/release-package.sh
+++ release/release-package.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 ref_type=$(jq '.ref_type' ${GITHUB_EVENT_PATH})
+tag_name=$(jq '.ref' ${GITHUB_EVENT_PATH})
+target_commitish=$GITHUB_REF
+release="release-${tag_name}"
+body="release-${tag_name}"
+
+generate_release()
+{
+  cat <<EOF
+{
+  "tag_name": "$tag_name",
+  "target_commitish": "$target_commitish",
+  "name": "$release",
+  "body": "$text",
+  "draft": false,
+  "prerelease": false
+}
+EOF
+}
 
 if [[ $ref_type == "\"tag"\" ]]
 then
-  echo "its a tag"
-  python setup.py bdist_wheel
-  echo "**************"
-  ls
-  echo "**************"
-  ls dist
+  echo "Creating release"
+  curl --data "$(generate_post_data)" "https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$GITHUB_TOKEN"
+
   exit 0
 else
   echo "it is not a tag"
   # do not continue the workflow
-  exit 42
+  exit 0
 fi
